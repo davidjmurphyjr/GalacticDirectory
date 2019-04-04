79b1a2a2e326afe5fc42eecca96d4ae5d4192917
David Murphy <dmurphy@gradifi.com>
2019-04-04 18:08:35 -0400

    loading indicator

--- app/app.js
+++ app/app.js
@@ -5,5 +5,15 @@ angular.module('galacticDirectory', ['ngRoute', 'galacticDirectory.people'])
     .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
         $locationProvider.hashPrefix('!');
         $routeProvider.otherwise({redirectTo: '/people/1'});
+    }])
+
+    .controller('LoadingCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+        $rootScope.$on('$routeChangeStart', function() {
+            $scope.isRouteLoading = true;
+        });
+
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $scope.isRouteLoading = false;
+        });
     }]);
 
